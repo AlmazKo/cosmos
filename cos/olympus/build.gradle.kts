af424d9ea4b55072a2327252a87094c13816d726
Almazko <a.s.suslov@gmail.com>
2020-12-12 21:42:11 +0300

    support running olympus by idea

--- cos/olympus/build.gradle.kts
+++ cos/olympus/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 java.modularity.inferModulePath.set(true)
 
 dependencies {
-    compileOnly("org.jetbrains:annotations:20.1.0")
+    implementation("org.jetbrains:annotations:20.1.0")
     implementation(project(":ops"))
     implementation(project(":map"))
     implementation(project(":logging"))
